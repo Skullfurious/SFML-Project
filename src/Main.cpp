5c35e92aed042cf5e29fda44fa11447d136d494b
Skullfurious <skullfurious@gmail.com>
2022-05-21 08:33:05 -0300

    Added Util.hpp/cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Util.hpp"
 
 int main()
 {
+    std::cout << Util::Vector3(0,0,0).add(1.5,1.0,1.0).m_x << std::endl;
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
