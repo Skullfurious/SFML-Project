b2ef535e036e6a0b36fd754acdf1473666152289
Skullfurious <skullfurious@gmail.com>
2022-05-30 02:39:39 -0300

    Further developing the Transform component

--- src/components/Transform2D.hpp
+++ src/components/Transform2D.hpp
@@ -1,3 +1,12 @@
 #include <vector>
 #include "../Component.hpp"
+#include "../utilities/Vector2.hpp"
 
+class Transform2D : public Component {
+    public:
+        Transform2D(Vector2<float> l, Vector2<float> s, Vector2<float> r) : location(l), rotation(r), scale(s) {}
+ 
+        Vector2<float> location;
+        Vector2<float> scale;
+        Vector2<float> rotation;
+};
